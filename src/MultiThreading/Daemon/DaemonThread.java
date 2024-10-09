984ef8dba63db7f37349dcba5607fb51237f79e0
Ujjwal <ujjwalshri86@gmail.com>
2024-10-09 18:44:52 +0530

    MultiThreading

---
+++ src/MultiThreading/Daemon/DaemonThread.java
@@ -0,0 +1,16 @@
+package MultiThreading.Daemon;
+class MyThread extends Thread{
+
+}
+
+public class DaemonThread {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().isDaemon());
+//        Thread.currentThread().setDaemon(true);//.IllegalThreadStateException
+        MyThread mt=new MyThread();
+        System.out.println(mt.isDaemon());
+        mt.setDaemon(true);
+        System.out.println(mt.isDaemon());
+
+    }
+}
